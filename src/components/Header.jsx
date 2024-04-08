fbfb099a5f6544bf68289780cec575300065961b
Brendan <brendansym@gmail.com>
2024-04-08 11:49:54 +0200

    init vite project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo"></div>
+      <nav></nav>
+      <div className="social-nav"></div>
+    </div>
+  );
+}
+
+export default Header;
