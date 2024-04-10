a8edde24b8c2d1876ad927ef22bc6977f8b0621b
Brendan <brendansym@gmail.com>
2024-04-10 12:00:01 +0200

    Header component initiate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,38 @@
+const LinkList = [
+  {
+    id: "00",
+    name: "home",
+    text: "Home",
+    url: ""
+  },
+  {
+    id: "01",
+    name: "about",
+    text: "About Us",
+    url: ""
+  },
+  {
+    id: "03",
+    name: "services",
+    text: "Our Services",
+    url: ""
+  },
+  {
+    id: "04",
+    name: "cta",
+    text: "Get a Quote",
+    url: ""
+  },
+]
+
 function Header() {
   return (
     <div className="header">
-      <div className="logo"></div>
-      <nav></nav>
-      <div className="social-nav"></div>
+      <div className="header-container">
+        <img src="./QIncite_Busines/src/assets/QIncite_logo.png" alt="QIncite Business" />
+        <nav></nav>
+        <div className="social-nav"></div>
+      </div>
     </div>
   );
 }
