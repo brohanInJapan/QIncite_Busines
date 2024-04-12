81091b8de457fefbcaf78530e9b0fc7b9fe987ce
Brendan <brendansym@gmail.com>
2024-04-12 15:36:23 +0200

    Cleun up

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -20,6 +20,7 @@ function Slide({ slide }) {
       style={{
         background: `linear-gradient(to top, rgba(247, 247, 247, 0.9), rgba(247, 247, 247, 0.9)), url(${slide.src}) no-repeat`,
         backgroundSize: `cover`,
+        backgroundBlendMode: "hard-light",
       }}
     >
       <h2>What others say</h2>
