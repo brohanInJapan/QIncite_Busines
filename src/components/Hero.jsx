cfe2fb8716f73f81be96d9646de43ce9bf9ffd3d
Brendan <brendansym@gmail.com>
2024-04-12 15:54:18 +0200

    Hero hotfix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
+import HeroShape from "../assets/vectors/Hero Shape.svg";
+
 function Hero({ children, img, element }) {
   return (
     <section
       className="hero"
       style={{
-        background: `linear-gradient(to top, rgba(30, 43, 41, 0.50), rgba(30, 43, 41, 0.50)), bottom / contain no-repeat url("src/assets/vectors/Hero Shape.svg"), center / cover no-repeat url(${img}) `,
+        background: `linear-gradient(to top, rgba(30, 43, 41, 0.50), rgba(30, 43, 41, 0.50)), bottom / contain no-repeat url(${HeroShape}), center / cover no-repeat url(${img}) `,
         backgroundBlendMode: "multiply",
       }}
     >
